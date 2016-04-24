031dcc2e213fb9ef14bf653d42a778022dbb0cae
summer2sama <wuyangthu11@gmail.com>
2016-04-24 13:35:03 -0500

    Fix Bug in HandleUpdate()

--- src/libkademlia/libkademlia.go
+++ src/libkademlia/libkademlia.go
@@ -129,6 +129,7 @@ func (e *CommandFailed) Error() string {
 
 func (k *Kademlia) DoPing(host net.IP, port uint16) (*Contact, error) {
 	// TODO: Implement
+<<<<<<< Updated upstream
   addr := fmt.Sprintf("%v:%v", host, port)
 	//hostname,port_str,err := net.SplitHostPort(addr)
 	port_str := fmt.Sprintf("%v", port)
@@ -142,6 +143,15 @@ func (k *Kademlia) DoPing(host net.IP, port uint16) (*Contact, error) {
 			}
 	}
 	*/
+=======
+  //addr := fmt.Sprintf("%v:%v", host, port)
+	//port_str := fmt.Sprintf("%v", port)
+	port_str := strconv.Itoa(int(port))
+	addr := host.String() + ":" + port_str
+	path := rpc.DefaultRPCPath + "localhost" + port_str
+	fmt.Println(addr)
+	fmt.Println(path)
+>>>>>>> Stashed changes
   client, err := rpc.DialHTTPPath("tcp", addr, path)
 	if err != nil{
 		  fmt.Println("Im here")
@@ -151,11 +161,14 @@ func (k *Kademlia) DoPing(host net.IP, port uint16) (*Contact, error) {
 	fmt.Println("passed 1")
 	defer client.Close()
   ping := PingMessage{k.SelfContact, NewRandomID()}
+	//fmt.Println("ping", ping)
 	var pong PongMessage
+	//fmt.Println("pong", pong)
 	err = client.Call("KademliaRPC.Ping", ping, &pong)
 	if err != nil{
 		  return nil, err
 	}
+	//fmt.Println("pong", pong)
 	k.Update(pong.Sender)
 	return &pong.Sender, nil
 	//return nil, &CommandFailed{
@@ -219,16 +232,20 @@ func (k *Kademlia) Update(c Contact) {
 func (k *Kademlia) HandleUpdate() {
 	for {
 		c := <- k.updateChan
+		//fmt.Println("New Contact to Update:",c)
+		//fmt.Println("Original Kademlia:", k)
 		bucketIndex := k.FindBucket(c.NodeID)
-		kb := k.table[bucketIndex]
+		//fmt.Println("bucketIndex:", bucketIndex)
+		kb := &k.table[bucketIndex]
+		//fmt.Println("Original kbucket:", kb)
 		contains, i := kb.FindContactInKBucket(c)
 		if contains {
 			kb.MoveToTail(i)
 		} else {
-				if len(kb) < cap(kb) {
+				if len(*kb) < cap(*kb) {
 					kb.AddToTail(c)
 				} else {
-					head := kb[0]
+					head := (*kb)[0]
 					_, err := k.DoPing(head.Host, head.Port)
 					if err != nil {
 						kb.Remove(0)
@@ -236,6 +253,8 @@ func (k *Kademlia) HandleUpdate() {
 					}
 				}
 		}
+		//fmt.Println("Updated kbucket:", kb)
+		//fmt.Println("Updated Kademlia:", k)
 	}
 }
 
