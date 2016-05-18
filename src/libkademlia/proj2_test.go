5154b733f6479856944865224af9c1e03a55f9c0
luofei11 <luofeithu@foxmail.com>
2016-05-18 13:58:17 -0500

    test

--- src/libkademlia/proj2_test.go
+++ src/libkademlia/proj2_test.go
@@ -3,14 +3,64 @@ package libkademlia
 import (
 	//"bytes"
 	//"net"
-	//"strconv"
+	"strconv"
 	"testing"
-	//"time"
+	"time"
 	"fmt"
 	"reflect"
 )
 
+var testPort uint16 = 3000
 
+const testAddr = "localhost"
+const divNum = 3
+
+type KademliaList []*Kademlia
+
+func GenerateRandomIDList(num int) (ret []ID) {
+	ret = make([]ID, num)
+	for i := 0; i < num; i++ {
+		ret[i] = NewRandomID()
+	}
+	return
+}
+
+func GenerateTreeIDList(num int) (ret []ID) {
+	ret = make([]ID, num)
+	ret[0] = NewRandomID()
+	for i := 1; i < num; i++ {
+		if i > 150 {
+			ret[i] = NewRandomID()
+		} else {
+			curID := ret[i/divNum]
+			curID[i/8] = curID[i/8] ^ (1 << uint8(7-(i%8)))
+			ret[i] = curID
+		}
+	}
+	return ret
+}
+
+func GenerateTestList(num int, idList []ID) (kRet KademliaList, cRet []Contact) {
+	kRet = []*Kademlia{}
+	cRet = []Contact{}
+	for i := 0; i < num; i++ {
+		laddr := testAddr + ":" + strconv.Itoa(int(testPort))
+		testPort++
+		var k *Kademlia
+		if idList != nil && i < len(idList) {
+			k = NewKademliaWithId(laddr, idList[i])
+		} //else {
+			//k = NewKademliaWithId(laddr, nil)
+		//}
+		cRet = append(cRet, k.SelfContact)
+		kRet = append(kRet, k)
+	}
+	return
+}
+
+func (ks KademliaList) ConnectTo(k1, k2 int) {
+	ks[k1].DoPing(ks[k2].SelfContact.Host, ks[k2].SelfContact.Port)
+}
 func TestIterativeFindNode(t *testing.T) {
 	instance1 := NewKademlia("localhost:7950")
 	instance2 := NewKademlia("localhost:7951")
@@ -49,7 +99,41 @@ func TestIterativeFindNode(t *testing.T) {
 		t.Error("Didn't Find All in DoIterativeFindNode!")
 	}
 }
+func TestIterativeFindValue(t *testing.T) {
+	kNum := 120
+	targetIdx := kNum - 23
+	treeList := GenerateTreeIDList(kNum)
+	kList, _ := GenerateTestList(kNum, treeList)
+	for i := 1; i < kNum; i++ {
+		kList.ConnectTo(i, i/divNum)
+	}
+	time.Sleep(100 * time.Millisecond)
+	searchKey := kList[targetIdx].SelfContact.NodeID
+	searchKey[IDBytes-1] = 0
+	randValue := []byte(NewRandomID().AsString())
+	kList[targetIdx/divNum].DoStore(&kList[targetIdx].SelfContact, searchKey, randValue)
+	time.Sleep(3 * time.Millisecond)
+	retVal, _ := kList[targetIdx].LocalFindValue(searchKey)
+	if retVal == nil {
+		t.Error("The target node should have the key/value pair")
+		return
+	}
+	if string(retVal) != string(randValue) {
+		t.Error("The stored value should equal to each other")
+		return
+	}
+	res, _ := kList[0].DoIterativeFindValue(searchKey)
+	if res == nil {
+		t.Error("The coressponding value should be found")
+		return
+	}
+	if string(res) != string(randValue) {
+		t.Error("Search result doesn't match: " + string(res) + "!=" + string(randValue))
+	}
+	t.Log("TestIterativeFindValue done successfully!\n")
+	return
 
+}
 func TestIterativeStore(t *testing.T) {
 	instance1 := NewKademlia("localhost:3456")
 	instance2 := NewKademlia("localhost:4567")
